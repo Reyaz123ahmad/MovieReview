43a8dc429138e03f5d7d03ed11f5bb3dbb8277bf
Reyaz Ahmad <reyazahmad40544@gmail.com>
2025-09-02 00:34:12 +0530

    Seventh commit

--- movieReview-backend/server.js
+++ movieReview-backend/server.js
@@ -31,7 +31,7 @@ app.use(
             'http://localhost:5177',
             'https://localhost:5173',
             'https://localhost:5177',
-            'https://movie-review-azure.vercel.app/'
+            'https://movie-review-azure.vercel.app'
             
         ],
         credentials: true
